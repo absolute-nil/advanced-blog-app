ceadcd8196f62e843285122a1083224369b1ce5c
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-09-28 13:52:59 +0530

    setup chromium browser for tests

---
+++ tests/header.test.js
@@ -0,0 +1,25 @@
+//! Later use selenium for integration tests
+
+const puppeteer = require('puppeteer');
+
+let browser;
+let page;
+
+beforeEach(async () => {
+  browser = await puppeteer.launch({
+    headless: false,
+  });
+
+  page = await browser.newPage();
+  await page.goto('localhost:3000');
+});
+
+afterEach(async () => {
+  await browser.close();
+});
+
+test('we can launch a browser', async () => {
+  const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
+
+  expect(text).toEqual('Blogster');
+});
