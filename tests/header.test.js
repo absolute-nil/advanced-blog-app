478ce4b93c038923f9ae91944e3925536fc5b508
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-09-30 23:33:16 +0530

    finish abstraction and authentication

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,21 +1,17 @@
+Number.prototype._called = {};
+
 //! Later use selenium for integration tests
-const puppeteer = require('puppeteer');
-const sessionFactory = require('./factories/sessionFactory');
-const userFactory = require('./factories/userFactory');
-let browser;
+const Page = require('./helpers/page');
+
 let page;
 
 beforeEach(async () => {
-  browser = await puppeteer.launch({
-    headless: false,
-  });
-
-  page = await browser.newPage();
+  page = await Page.build();
   await page.goto('localhost:3000');
 });
 
 afterEach(async () => {
-  await browser.close();
+  await page.close();
 });
 
 test('check if header text is correct', async () => {
@@ -33,14 +29,7 @@ test('on login click should lead to oauth flow', async () => {
 });
 
 test('when signed in shows logout button', async () => {
-  const user = await userFactory();
-  const { session, sig } = sessionFactory(user);
-
-  await page.setCookie({ name: 'session', value: session });
-  await page.setCookie({ name: 'session.sig', value: sig });
-  await page.goto('localhost:3000');
-
-  await page.waitFor('a[href="/auth/logout"]');
+  await page.login();
 
   const text = await page.$eval('a[href="/auth/logout"]', (el) => el.innerHTML);
 
