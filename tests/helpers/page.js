a128934cf9c3fdcf8f6cf4ca8bcfa20d85eb0c40
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-10-02 19:59:03 +0530

    create get and post functions

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -38,6 +38,42 @@ class CustomPage {
   async getContentsOf(selector) {
     return await this.page.$eval(selector, (el) => el.innerHTML);
   }
+
+  get(path) {
+    // path cant be accessed directly like fetch(path, because evaluate
+    // will turn the request into json so the value of path
+    // will not be there as it is in our local machine not in browser
+    // because it executed in the browser
+    // to solve this you will have to provide path as a second
+    // argument like }, path); and then accept it in argument
+    // like .evaluate((_path) => {
+    return this.page.evaluate((_path) => {
+      return fetch(_path, {
+        method: 'GET',
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }).then((res) => res.json());
+    }, path);
+  }
+
+  post(path, data) {
+    return this.page.evaluate(
+      (_path, _data) => {
+        return fetch(_path, {
+          method: 'POST',
+          credentials: 'same-origin',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(_data),
+        }).then((res) => res.json());
+      },
+      path,
+      data
+    );
+  }
 }
 
 module.exports = CustomPage;
